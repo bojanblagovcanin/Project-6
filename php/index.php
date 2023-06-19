36105dd059503c3e26dce9e3d06f5ec01301568c
Sorrybro467 <selmanbursal0@gmail.com>
2023-06-19 16:40:04 -0400

    try something something

--- php/index.php
+++ php/index.php
@@ -20,16 +20,18 @@ function update_elevatorNetwork(int $node_ID, int $new_floor = 1): int
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// Check if a specific form field is submitted
 
-	$floor = $_POST['floor'];
+	$floor = $_POST['data'];
 		
 		if ($floor === '1'){
 			echo "floor 1 go";
 			update_elevatorNetwork(1, 1);
 		}
 		if ($floor === '2'){
+			echo "floor 2 go";
 			update_elevatorNetwork(1, 2);
 		}
 		if ($floor === '3'){
+			echo "floor 3 go";
 			update_elevatorNetwork(1, 3);
 		}
 	}
