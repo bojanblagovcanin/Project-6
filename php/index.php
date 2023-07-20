775a6874552df04e5ebd42e933636e4a8d0671f4
Sorrybro467 <selmanbursal0@gmail.com>
2023-07-20 11:07:32 -0400

    changed how data is saved

--- php/index.php
+++ php/index.php
@@ -12,17 +12,22 @@ function update_elevatorNetwork(int $node_ID, int $new_floor = 1): int
     $statement->execute();
 
     return $new_floor;
-}
+}   
 
 function save_floor_and_time(int $floor, int $node_ID)
 {
     $db2 = new PDO('mysql:host=127.0.0.1;dbname=floordata', 'ese', 'ese');
-    $query = 'INSERT INTO floor_log (nodeID, floor, time_called) 
-              VALUES (:nodeID, :floor, :time_called)';
+    
+    $weekday = date('N'); // 'N' returns the ISO-8601 numeric representation of the day of the week (1 for Monday, 7 for Sunday)
+    $hour = date('G'); // 'G' returns the 24-hour format of the hour (0 to 23)
+    
+    $query = 'INSERT INTO floor_log (nodeID, floor, weekday, hour) 
+              VALUES (:nodeID, :floor, :weekday, :hour)';
     $statement = $db2->prepare($query);
     $statement->bindValue('nodeID', $node_ID);
     $statement->bindValue('floor', $floor);
-    $statement->bindValue('time_called', date('Y-m-d H:i:s'));
+    $statement->bindValue('weekday', $weekday);
+    $statement->bindValue('hour', $hour);
     $statement->execute();
 }
 
