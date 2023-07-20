3a738a67292ca749ca6d86aab3f15ecfad6ab3b0
Sorrybro467 <selmanbursal0@gmail.com>
2023-07-20 10:16:10 -0400

    added php code to save everyfloor called from demo pg

--- php/index.php
+++ php/index.php
@@ -14,9 +14,22 @@ function update_elevatorNetwork(int $node_ID, int $new_floor = 1): int
     return $new_floor;
 }
 
+function save_floor_and_time(int $floor, int $node_ID)
+{
+    $db2 = new PDO('mysql:host=127.0.0.1;dbname=floordata', 'ese', 'ese');
+    $query = 'INSERT INTO floor_log (nodeID, floor, time_called) 
+              VALUES (:nodeID, :floor, :time_called)';
+    $statement = $db2->prepare($query);
+    $statement->bindValue('nodeID', $node_ID);
+    $statement->bindValue('floor', $floor);
+    $statement->bindValue('time_called', date('Y-m-d H:i:s'));
+    $statement->execute();
+}
+
 ?>
 
 <?php
 $floor = $_GET['floor'];
+save_floor_and_time($floor, 1);
 $curFlr = update_elevatorNetwork(1, $floor);
 ?>
